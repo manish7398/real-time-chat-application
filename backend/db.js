3d068bbaaac7dc2e5610e984104c39f794dfa696
Manish <palmani0000@gmail.com>
2026-01-30 17:48:39 +0530

    Fixed MongoDB connection and setup local database

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
+  
     console.error("DB Error:", error.message);
     
   }
